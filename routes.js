ffd8ec44471d3f9bf5c8c75c27327d9472fcca12
pamela vieira da silva <panhavsilva@gmail.com>
2021-03-27 17:13:17 -0300

    add eslint

--- routes.js
+++ routes.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const routes = express.Router();
-const products = require('./controllers/ProductsController');
+const express = require('express')
+const routes = express.Router()
+const products = require('./controllers/products-controller')
 
-routes.get('/products', products.list);
-routes.get('/products/:id/show', products.show);
+routes.get('/products', products.list)
+routes.get('/products/:id/show', products.show)
 
-routes.post('/products', products.post);
-routes.put('/products/:id', products.put);
-routes.delete('/products/:id', products.delete);
+routes.post('/products', products.post)
+routes.put('/products/:id', products.put)
+routes.delete('/products/:id', products.delete)
 
-module.exports = routes;
+module.exports = routes
--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const methodOverride = require('method-override');
-const routes = require('./routes');
+const express = require('express')
+const cors = require('cors')
+const methodOverride = require('method-override')
+const routes = require('./routes')
 
-const server = express();
+const server = express()
 
 const corsOptions = {
-  origin : 'http://127.0.0.1:5500',
+  origin: 'http://127.0.0.1:5500',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSucessStatus: 204
-};
+}
 
-server.use(express.json());
-server.use(express.urlencoded( {extended: true} ));
-server.use(express.static('public'));
-server.use(methodOverride('_method'));
-server.use(cors(corsOptions));
-server.use(routes);
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+server.use(express.static('public'))
+server.use(methodOverride('_method'))
+server.use(cors(corsOptions))
+server.use(routes)
 
-
-server.listen(5000, function(){
-  console.log( 'server is running' );
-});
+server.listen(5000, function () {
+  console.log('server is running')
+})

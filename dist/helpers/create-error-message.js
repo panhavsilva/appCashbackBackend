834e43879970c2c23a0c6d778dc638f73f6e5695
pamela vieira da silva <panhavsilva@gmail.com>
2021-04-14 19:45:58 -0300

    gitignore - dist

--- dist/helpers/create-error-message.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createErrorMessage = void 0;
-function createErrorMessage(message) {
-    return { message: message, error: true };
-}
-exports.createErrorMessage = createErrorMessage;
-//# sourceMappingURL=create-error-message.js.map
--- dist/helpers/is-number.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isNumber = void 0;
-function isNumber(price) {
-    return typeof price === ('number');
-}
-exports.isNumber = isNumber;
-//# sourceMappingURL=is-number.js.map

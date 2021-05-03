f14be33f006403d02fa0a3c721ef3edeb0be74e9
pamela vieira da silva <panhavsilva@gmail.com>
2021-05-03 18:05:38 -0300

    update verification products of order

--- src/core/use-cases/order/create-order.ts
+++ src/core/use-cases/order/create-order.ts
@@ -4,9 +4,22 @@ import { Order } from '@/core/types/order'
 export type SaveOrder = (o: Order) => Promise<Order>
 type CreateOrder = (o: Order) => (f: SaveOrder) => Promise<Either<string, Order>>
 
+const hasProduct = (order: Order): Boolean => {
+  return order.product_list.length !== 0
+}
+const orderError = async (): Promise<never> => { throw new Error('Invalid Product!') }
+const orderProductsValid = (order: Order): Boolean => {
+  const orderItems = [hasProduct(order)]
+  return orderItems.every((item) => item === true)
+}
+const validOrder = async (order: Order): Promise<Order> => {
+  return orderProductsValid(order) ? order : orderError()
+}
+
 export const createOrder: CreateOrder = (order: Order) => async (saveOrder) => {
   try {
     const newOrder = await saveOrder(order)
+    await validOrder(order)
     return right(newOrder)
   } catch (e) {
     return left(e)
--- src/core/use-cases/order/order.test.ts
+++ src/core/use-cases/order/order.test.ts
@@ -13,29 +13,9 @@ const order2: Order = {
   order_value: 0,
   product_list: [],
 }
-const order3: Order = {
-  order_value: 100,
-  product_list: [{ name: 'product1', price: 0, quantity: 0 },
-    { name: 'product2', price: 200, quantity: 1 },
-  ],
-}
 
-const quantityIsZero = (order: Order): Boolean => {
-  const productList = order.product_list
-  return productList.every((item) => item.quantity > 0)
-}
-const hasProduct = (order: Order): Boolean => {
-  return order.product_list.length !== 0
-}
-const orderValid = (order: Order): Boolean => {
-  const orderItems = [
-    hasProduct(order),
-    quantityIsZero(order),
-  ]
-  return orderItems.every((item) => item === true)
-}
 const saveOrder: SaveOrder = async (order) => {
-  return orderValid(order) ? order : saveOrderError()
+  return order
 }
 const saveOrderError = async (): Promise<never> => {
   throw new Error('Invalid Order!')
@@ -48,12 +28,7 @@ it('Should create order', async () => {
 
 it('product_list = [] should throw error', async () => {
   const newOrder = await createOrder(order2)(saveOrder)
-  expect(newOrder).toEqual(left(new Error('Invalid Order!')))
-})
-
-it('quantity = 0 should throw error', async () => {
-  const newOrder = await createOrder(order3)(saveOrder)
-  expect(newOrder).toEqual(left(new Error('Invalid Order!')))
+  expect(newOrder).toEqual(left(new Error('Invalid Product!')))
 })
 
 it('should createOrder throw error', async () => {

8cfa3a9054e2655957111f4d8e9279da4b799596
pamela vieira da silva <panhavsilva@gmail.com>
2021-05-11 22:16:05 -0300

    add validation of value cashback

--- src/core/use-cases/cashback/create-cashback.ts
+++ src/core/use-cases/cashback/create-cashback.ts
@@ -4,10 +4,25 @@ import { Cashback } from '@/core/types/cashback'
 export type SaveCashback = (c: Cashback) => Promise<Cashback>
 type CreateCashback = (c: Cashback) => (f: SaveCashback) => Promise<Either<string, Cashback>>
 
+const isMinSmallMax = (c: Cashback): boolean => {
+  return c.min_value < c.max_value
+}
+const isCashbackValid = (c: Cashback): boolean => {
+  const cashbackItems = [isMinSmallMax(c)]
+  return cashbackItems.every((item) => item === true)
+}
+const validCashback = async (c: Cashback): Promise<Cashback> => {
+  if (isCashbackValid(c)) {
+    return c
+  }
+  throw new Error('Invalid Cashback!')
+}
+
 export const createCashback: CreateCashback = (cashback) => async (saveCashback) => {
   try {
-    const newProduct = await saveCashback(cashback)
-    return right(newProduct)
+    const newCashback = await validCashback(cashback)
+    const saveNewCashback = await saveCashback(newCashback)
+    return right(saveNewCashback)
   } catch (e) {
     return left(e)
   }

cca65279bd751a41083cded1a9e97f9636575468
pamela vieira da silva <panhavsilva@gmail.com>
2021-05-26 20:17:49 -0300

    update cashback message error

--- src/core/use-cases/cashback/cashback.test.ts
+++ src/core/use-cases/cashback/cashback.test.ts
@@ -1,11 +1,16 @@
-import { right, left } from 'fp-ts/Either'
+import { right, left, isLeft } from 'fp-ts/Either'
 import { createCashback, SaveCashback } from './create-cashback'
 import { Cashback } from '@/core/types/cashback'
 
 const cashback: Cashback = { minValue: 0, maxValue: 100, percentage: 5 }
 const cashback2: Cashback = { minValue: 100, maxValue: 0, percentage: 5 }
 const cashback3: Cashback = { minValue: 100, maxValue: 100, percentage: 5 }
-const saveCashback: SaveCashback = async (cashback: Cashback) => { return cashback }
+const saveCashback: SaveCashback = async (cashback) => {
+  if (!isLeft(cashback)) {
+    return cashback.right
+  }
+  throw cashback.left
+}
 const saveCashbackError = async (): Promise<never> => {
   throw new Error('Invalid Cashback!')
 }
@@ -17,12 +22,16 @@ it('Deve criar um cashback', async () => {
 
 it('Deve lançar um erro quando o minValue for maior que o maxValue', async () => {
   const newCashback = await createCashback(cashback2)(saveCashback)
-  expect(newCashback).toEqual(left(new Error('Invalid Cashback!')))
+  expect(newCashback).toEqual(
+    left(new Error('Invalid Cashback! - min value should be less then max value.'))
+  )
 })
 
 it('Deve lançar um erro quando o minValue for igual ao maxValue', async () => {
   const newCashback = await createCashback(cashback3)(saveCashback)
-  expect(newCashback).toEqual(left(new Error('Invalid Cashback!')))
+  expect(newCashback).toEqual(
+    left(new Error('Invalid Cashback! - min value should be less then max value.'))
+  )
 })
 
 it('Deve lançar um erro quando utilizado saveCashbackError', async () => {

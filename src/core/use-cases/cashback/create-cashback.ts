9b2395a68489460f628b3abed7861a767c3f7e31
pamela vieira da silva <panhavsilva@gmail.com>
2021-05-26 20:17:49 -0300

    update cashback message error

--- src/core/use-cases/cashback/create-cashback.ts
+++ src/core/use-cases/cashback/create-cashback.ts
@@ -1,21 +1,23 @@
 import { right, Either, left } from 'fp-ts/Either'
 import { Cashback } from '@/core/types/cashback'
 
-export type SaveCashback = (c: Cashback) => Promise<Cashback>
+export type SaveCashback = (c: Either<Error, Cashback>) => Promise<Cashback|never>
 type CreateCashback = (c: Cashback) => (f: SaveCashback) => Promise<Either<string, Cashback>>
 
 const isMinSmallMax = (cashback: Cashback): boolean => {
   return cashback.minValue < cashback.maxValue
 }
 const isCashbackValid = (cashback: Cashback): boolean => {
-  const cashbackItems = [isMinSmallMax(cashback)]
-  return cashbackItems.every((item) => item === true)
+  const cashbackValidators = [isMinSmallMax(cashback)]
+  return cashbackValidators.every((item) => item === true)
 }
-const validCashback = async (cashback: Cashback): Promise<Cashback> => {
+const validCashback = async (cashback: Cashback): Promise<Either<Error, Cashback>> => {
   if (isCashbackValid(cashback)) {
-    return cashback
+    return right(cashback)
   }
-  throw new Error('Invalid Cashback!')
+  return left(
+    new Error('Invalid Cashback! - min value should be less then max value.')
+  )
 }
 
 export const createCashback: CreateCashback = (cashback) => async (saveCashback) => {

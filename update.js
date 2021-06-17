0f945f734c4aed2aab49fd17193278262b723770
pamela vieira da silva <panhavsilva@gmail.com>
2021-06-17 17:39:18 -0300

    update dependencies

---
+++ update.js
@@ -0,0 +1,19 @@
+const { spawn } = require('cross-spawn')
+const pkg = require('../package.json')
+
+const dependencies = Object.keys(pkg.dependencies)
+const devDependencies = Object.keys(pkg.devDependencies)
+
+const add = (args) => {
+  return spawn('npm', ['install', '--save', '--save-exact']
+    .concat(args), { stdio: 'inherit' })
+}
+
+const addDev = (args) => {
+  return spawn('npm', ['install', '--save-dev', '--save-exact']
+    .concat(args), { stdio: 'inherit' })
+}
+
+add(dependencies).on('close', () => {
+  addDev(devDependencies).on('close', (code) => process.exit(code))
+})
